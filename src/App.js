9c298701b1a7e4f441824cd3cfc709e108505bc4
Sam Ervin <samuel.ervin@gmail.com>
2023-06-01 23:04:42 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-export default function Square() {
+import { useState } from 'react';
+
+export default function Board() {
   return (
     <>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
 }
+
+function Square({ value }) {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    console.log('clicked!');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
+
